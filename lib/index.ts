3a33f3a0b07428e87de8d60c70298f8f31c65fd4
BruceAu <bruceau00@gmail.com>
2020-08-04 18:57:59 +0800

    init

---
+++ lib/index.ts
@@ -0,0 +1,39 @@
+import { fork } from 'child_process'
+import * as url from 'url'
+
+export const debug = (app, port = 9229) => {
+  const nodeOptions = process.env.NODE_OPTIONS
+  if (nodeOptions !== '--inspect' && nodeOptions !== '--inspect-res') {
+    return;
+  }
+
+
+  console.debug(`
+
+    Debugger Proxy listening your app.
+
+    To start debugging, open the following URL in Chrome:
+
+    chrome://devtools/bundled/js_app.html?ws=YOUR_APP_HOST/__debug__
+
+  `);
+
+  process.env.NODE_OPTIONS = ''
+
+  const worker = fork('./dist/inspect/debug.js',)
+
+  process.env.NODE_OPTIONS = nodeOptions
+  
+  app.on('upgrade', function upgrade(request, socket) {
+    const pathname = url.parse(request.url).pathname;
+    if (pathname === '/__debug__') {
+      worker.send({ 
+        headers: request.headers, 
+        method: request.method, 
+        type: 'connect',
+        url: '/' + port,
+      }, socket);
+    }
+  });
+
+}
---
+++ lib/proxy.ts
@@ -0,0 +1,124 @@
+import * as WebSocket from "ws";
+import * as url from "url";
+import * as http from "http";
+
+type ProxySessionInfo = {
+  frontend: WebSocket;
+  backend: WebSocket;
+};
+
+const sessionCaches: Map<string, ProxySessionInfo> = new Map();
+let server: WebSocket.Server;
+
+// 获取本级debugger ws地址
+function getDebuggerUrl(prot: string) {
+  return new Promise<string>((resolve, reject) => {
+    http.get(`http://127.0.0.1:${prot}/json`, (res) => {
+      let rawInfo = "";
+      res.on("data", (chunk) => (rawInfo += chunk));
+      res
+        .on("end", () => {
+          const debugInfos = JSON.parse(rawInfo);
+          const connectCount = debugInfos.length;
+          if (connectCount > 1) {
+            reject(`${connectCount} debugger connects on this port`);
+            return;
+          }
+
+          const url = debugInfos[0].webSocketDebuggerUrl;
+          if (url) {
+            resolve(url);
+          } else {
+            reject(`a devTools has connected to ws://127.0.0.1/${prot}`);
+          }
+        })
+        .on("error", (error) => {
+          reject(error.message);
+        });
+    });
+  });
+}
+
+// 关闭转发代理
+function closeSession(prot: string) {
+  const session = sessionCaches.get(prot);
+  if (session) {
+    session.frontend?.close();
+    session.backend?.close();
+    sessionCaches.delete(prot);
+  }
+}
+
+// 创建一个转发连接
+function createBackendProxy(url: string, prot: string) {
+  return new Promise<WebSocket>((resolve, reject) => {
+    const backend = new WebSocket(url);
+    backend.on("open", () => {
+      console.debug("debugger proxy is created");
+      backend["_socket"].pause();
+      resolve(backend);
+    });
+    backend.on("error", (err) => {
+      reject(err.message);
+    });
+  });
+}
+
+// 代理数据转发
+function proxyMessage(frontend: WebSocket, backend: WebSocket, prot: string) {
+  frontend.on("message", (data) => {
+    backend?.send(data);
+  });
+  backend.on("message", (data) => {
+    frontend?.send(data);
+  });
+  frontend.on("close", () => {
+    closeSession(prot);
+  });
+  backend.on("close", () => {
+    closeSession(prot);
+  });
+  frontend["_socket"].resume();
+  backend["_socket"].resume();
+  sessionCaches.set(prot, { frontend, backend });
+}
+
+// 开始代理
+async function proxyStart(socket: WebSocket, prot: string) {
+  // 先清理一下旧的连接
+  closeSession(prot);
+
+  try {
+    const url = await getDebuggerUrl(prot);
+    const backend = await createBackendProxy(url, prot);
+    proxyMessage(socket, backend, prot);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+
+server = new WebSocket.Server({noServer: true});
+
+server.on("connection", (socket, incomingMessage) => {
+  // pause current socket wait proxy
+  socket["_socket"].pause();
+
+  // get proxy prot
+  const prot = url.parse(incomingMessage.url, true).pathname.slice(1);
+
+  proxyStart(socket, prot);
+});
+
+server.on("error", (e) => {
+  console.error(e.message);
+});
+
+process.on('message', (message, handle) => {
+  if (typeof message === 'object' && message.type === 'connect') {
+    server.handleUpgrade(message, handle, Buffer.from(''), function (conn) {
+      server.emit('connection', conn, message);
+    });
+  }
+})
+
